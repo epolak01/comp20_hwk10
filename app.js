fb776ab26b463c0c232dc4223045944b4236933d
epolak01 <56269269+epolak01@users.noreply.github.com>
2020-04-28 13:04:31 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 
 var express = require('express')
 var app = express()
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 var http = require('http');
 var url = require('url');
 
